2032c0fe3c1df3a22664f0c7c8830f9c9be922a7
lqjun <1120948897@qq.com>
2024-01-07 05:17:31 +0800

    chfloss训练脚本修改

--- scripts/chfloss/train_shha_2.sh
+++ scripts/chfloss/train_shha_2.sh
@@ -1,5 +1,5 @@
 python ./tools/train_cc.py  \
---cfg="configs/chfloss/SHHA_final_1.local.py" \
+--cfg="configs/chfloss/SHHA_final_2.local.py" \
 --local-rank=0 \
 --launcher="pytorch" \
 --debug=True
